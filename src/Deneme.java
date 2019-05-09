3c74c8d16c69295279660d527976bba80afd8b01
Tuğçe <tugcedvc@hotmail.com>
2019-05-09 22:33:38 +0300

    class

---
+++ src/Deneme.java
@@ -0,0 +1,11 @@
+
+public class Deneme {
+    public static void main(String[]args)
+    {
+        Deneme1 d=new Deneme1();
+
+        System.out.println(d.Topla(4,5));
+
+    }
+}
+
---
+++ src/Deneme1.java
@@ -0,0 +1,10 @@
+public class Deneme1 {
+
+        public  int Topla(int a,int b)
+        {
+            int tpl=a+b;
+            return tpl;
+        }
+
+
+}
